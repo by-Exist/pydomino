cd4ef28c247e033ee0c44b26b0abed9fb0f595e8
by-Exist <bolk9652@naver.com>
2022-11-22 14:02:04 +0900

    Block add __init_subclass__
    - Callable 프로토콜의 한계로 인해 fall_down 메서드의 인자 형태를 타이핑 할 수 없어, fall_down이라는 속성이 필요하다는 것만을 Block 인터페이스로 제공하였음.
    - __init_subclass__ 를 통해 서브클래스에 fall_down이 정의되었는지, 그것이 Callable인지 검증하는 과정을 추가함.

--- pydomino/block.py
+++ pydomino/block.py
@@ -4,8 +4,22 @@ P = ParamSpec("P")
 R_co = TypeVar("R_co", covariant=True)
 
 
+class RequiredFallDownMethod(RuntimeError):
+    ...
+
+
+class FallDownIsNotMethod(RuntimeError):
+    ...
+
+
 class Block(Protocol):
     fall_down: Any
+
+    def __init_subclass__(cls) -> None:
+        if not hasattr(cls, "fall_down"):
+            raise RequiredFallDownMethod(f"{cls.__name__}에 fall_down 메서드가 정의되어 있지 않습니다.")
+        if not callable(getattr(cls, "fall_down", None)):
+            raise FallDownIsNotMethod(f"{cls.__name__}에 정의된 fall_down은 메서드여야 합니다.")
 
 
 class Falldownable(Protocol[P, R_co]):
