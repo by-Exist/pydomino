3de5eda75c7447bcf30c89da9f8adea2923beab4
by-Exist <bolk9652@naver.com>
2022-11-25 19:04:12 +0900

    Remove Block.__init_subclass__
    - Block의 서브 클래스를 Command, Event 등의 형태로 정의했을 때 걸림돌이 되리라는 점을 생각못했다.

--- pydomino/block.py
+++ pydomino/block.py
@@ -15,12 +15,6 @@ class FallDownIsNotMethod(RuntimeError):
 class Block(Protocol):
     fall_down: Any
 
-    def __init_subclass__(cls) -> None:
-        if not hasattr(cls, "fall_down"):
-            raise RequiredFallDownMethod(f"{cls.__name__}에 fall_down 메서드가 정의되어 있지 않습니다.")
-        if not callable(getattr(cls, "fall_down", None)):
-            raise FallDownIsNotMethod(f"{cls.__name__}에 정의된 fall_down은 메서드여야 합니다.")
-
 
 class Falldownable(Protocol[P, R_co]):
     def fall_down(
