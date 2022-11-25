1d6e9be6bc3c8229b144f3bebffdbefc95b62b55
by-Exist <bolk9652@naver.com>
2022-11-25 18:59:18 +0900

    Remove message module
    - Message 시스템을 Pydomino에서 제공하지 않기로 결정했다.
    - 원한다면 직접 구현해서 사용하면 되니까.

--- pydomino/message/__init__.py
+++
@@ -1,5 +0,0 @@
-from .consumer.base import MessageConsumer as MessageConsumer
-from .message import Deserializer as Deserializer
-from .message import Message as Message
-from .message import Serializer as Serializer
-from .producer.base import MessageProducer as MessageProducer
--- pydomino/message/producer/base.py
+++
@@ -1,7 +0,0 @@
-from typing import Protocol
-from ..message import Serializer
-
-
-class MessageProducer(Protocol):
-    async def produce(self, serializable: Serializer) -> None:
-        ...
